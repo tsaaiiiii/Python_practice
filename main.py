1a73490b6b518080c298ffaf3eb32732a4cd7609
tsaaiiiii <a0909513991@gmail.com>
2025-08-16 19:43:04 +0800

    feat: 新增練習資料驗證

--- main.py
+++ main.py
@@ -8,7 +8,7 @@
 
 # 基本路由設定範例
 from typing import List
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Path
 
 app = FastAPI() # FastAPI 物件放在變數 app 裡面
 
@@ -40,7 +40,7 @@ def squareNumber(number:int):
     squared_value  = number**2
     return { "origin" : number, "squared": squared_value }
 
-# practice 2 - 路由與要求字串的練習
+# practice 2 - 路由與「要求字串」的練習
 # 處理路徑 /greet?name=John
 @app.get("/greet")
 def greet(name):
@@ -56,3 +56,53 @@ def multiply(num1: int, num2: int):
 @app.get("/product")
 def product(id: List[int] = Query(...)):
     return {"ids": id}
+
+# 輸入資料驗證
+# item_id: Annotated[int, None] 表示 item_id 是 int 類型，且沒有其他限制
+# 接收浮點數：float
+# 更多限制：
+#   item_id:Annotated[int,Path(gt=3,lt=10)]
+#   Path 代表針對路徑參數做驗證
+#   gt : > | lt : < | ge : >= | le : <=
+
+# 加上資料驗證的好處，假設打了 getItem 但是 item_id 傳入了字串，就會回應以下錯誤，並且會被擋掉，不會進到主要的程式
+# 如果沒有加資料驗證，就會是 內部伺服器錯誤 (500 Internal Server Error)，因為程式會嘗試將字串轉換為 float，失敗後沒有處理這個錯誤。
+# 422 Unprocessable Entity
+# {
+#   "detail": [
+#     {
+#       "type": "float_parsing",
+#       "loc": [
+#         "path",
+#         "item_id"
+#       ],
+#       "msg": "Input should be a valid number, unable to parse string as a number",
+#       "input": "abc"
+#     }
+#   ]
+# }
+
+from typing import Annotated
+@app.get("/item/{item_id}")
+def getItem(item_id:Annotated[float,None]):
+    id = float(item_id)
+    return { "result": id }
+
+@app.get("/item/{item_id}")
+def getItem2(item_id: Annotated[int, Path(gt=3, lt=100)]):
+    return { "result":  item_id }
+
+# /items?store_id=1&item_id=10
+# Query 代表針對查詢參數做驗證
+@app.get("/items")
+def getItems(
+    store_id:Annotated[int,Query(gt=0)],
+    item_id:Annotated[int,Query(gt=0, le=100)]
+):
+    
+    return {"result": f"{store_id} , {item_id}"}
+
+# 練習：處理路徑 /echo/{name}
+@app.get("/echo/{name}")
+def echoName(name:Annotated[str,Path(min_length=1, max_length=10)]):
+    return {"echo": name}
